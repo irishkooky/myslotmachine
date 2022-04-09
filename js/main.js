3c26aeb184f8fa936e0bcb33384793e9a5e9f6ba
irishkooy <marines.qvcmarin@gmail.com>
2022-04-09 19:10:09 +0900

    fix: スタートストップ機能追加

--- js/main.js
+++ js/main.js
@@ -9,29 +9,28 @@
       this.img.src = this.getRandomImage();
 
       this.timeoutId = undefined;
-
-      this.stop = document.createElement('div');
-      this.stop.textContent = 'STOP';
-      this.stop.classList.add('stop', 'inactive');
-      this.stop.addEventListener('click', () => {
-        if (this.stop.classList.contains('inactive')) {
+      this.spinStop = document.createElement('div');
+      this.spinStop.textContent = 'もういちどふる';
+      this.spinStop.setAttribute('id', 'spin');
+      this.spinStop.addEventListener('click', () => {
+        if (this.spinStop.classList.contains('inactive')) {
+          clearTimeout(this.timeoutId);
+          this.spinStop.classList.remove('inactive');
+          this.spinStop.textContent = 'もういちどふる';
           return;
         }
-        this.stop.classList.add('inactive');
-
-        clearTimeout(this.timeoutId);
-
-        panelsLeft--;
-
-        if (panelsLeft === 0) {
-          checkResult();
-          spin.classList.remove('inactive');
-          panelsLeft = 3;
-        }
+        this.spinStop.classList.add('inactive');
+        this.spin();
+        this.spinStop.textContent = 'ストップ';
       });
 
+      this.drawCard = document.createElement('div');
+      this.drawCard.setAttribute('id', 'draw-card');
+      this.drawCard.textContent = 'カードをひく';
+
       section.appendChild(this.img);
-      section.appendChild(this.stop);
+      section.appendChild(this.spinStop);
+      section.appendChild(this.drawCard);
 
       const main = document.querySelector('main');
       main.appendChild(section);
@@ -39,64 +38,21 @@
 
     getRandomImage() {
       const images = [
-        'img/seven.png',
-        'img/bell.png',
-        'img/cherry.png',
+        'img/1.jpg',
+        'img/2.jpg',
+        'img/3.jpg',
+        'img/4.jpg',
+        'img/5.jpg',
+        'img/6.jpg',
       ];
       return images[Math.floor(Math.random() * images.length)];
     }
-
     spin() {
       this.img.src = this.getRandomImage();
       this.timeoutId = setTimeout(() => {
         this.spin();
-      }, 50);
-    }
-
-    isUnmatched(p1, p2) {
-      return this.img.src !== p1.img.src && this.img.src !== p2.img.src;
-    }
-
-    unmatch() {
-      this.img.classList.add('unmatched');
-    }
-
-    activate() {
-      this.img.classList.remove('unmatched');
-      this.stop.classList.remove('inactive');
+      }, 100);
     }
   }
-
-  function checkResult() {
-    if (panels[0].isUnmatched(panels[1], panels[2])) {
-      panels[0].unmatch();
-    }
-    if (panels[1].isUnmatched(panels[0], panels[2])) {
-      panels[1].unmatch();
-    }
-    if (panels[2].isUnmatched(panels[0], panels[1])) {
-      panels[2].unmatch();
-    }
-  }
-
-  const panels = [
-    new Panel(),
-    new Panel(),
-    new Panel(),
-  ];
-
-  let panelsLeft = 3;
-
-  const spin = document.getElementById('spin');
-  spin.addEventListener('click', () => {
-    if (spin.classList.contains('inactive')) {
-      return;
-    }
-    spin.classList.add('inactive');
-
-    panels.forEach(panel => {
-      panel.activate();
-      panel.spin();
-    });
-  });
+  new Panel();
 }
